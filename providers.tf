687ce24d57a5c1ebca23e2ffa345332259f901e0
Bradz667 <62442850+bradsorph@users.noreply.github.com>
2025-12-04 13:58:41 +0100

    Refactor AAP provider configuration in providers.tf

--- providers.tf
+++ providers.tf
@@ -6,19 +6,19 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
-  }
   
-provider "aap" {
-  host = "https://caap.fvz.ansible-labs.de/" 
+  
+    provider "aap" {
+        host = "https://caap.fvz.ansible-labs.de/" 
 
-  #token = "my-aap-token" # Also supportsAAP_TOKEN environment variable
+        # token = "my-aap-token" # Also supportsAAP_TOKEN environment variable
 
-  # Basic authentication is also supported, ignored if token is set
-  #username = "my-aap-username" # Also supports AAP_USERNAME environment variable
-  #password = "my-aap-password" # Also supports AAP_PASSWORD environment variable
+        # Basic authentication is also supported, ignored if token is set
+        #username = "my-aap-username" # Also supports AAP_USERNAME environment variable
+        #password = "my-aap-password" # Also supports AAP_PASSWORD environment variable
+    }
 }
 }
-
 # Configure the AWS Provider
 provider "aws" {
   region = var.region
