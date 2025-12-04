4d7a1e7311acd9548187649d95cc3695ffa3d93b
Bradz667 <62442850+bradsorph@users.noreply.github.com>
2025-12-04 14:38:55 +0100

    Add AAP inventory resource for testing

--- aap_inventory.tf
+++ aap_inventory.tf
@@ -1,1 +1,10 @@
-
+resource "aap_inventory" "bradz_test_inv" {
+  name         = "BRADZ667 - Inventory TF"
+  description  = "A inventory for testing"
+  organization = 2
+  variables = jsonencode(
+    {
+      "foo-key" : "bar-value"
+    }
+  )
+}
