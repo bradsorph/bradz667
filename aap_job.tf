d2373d00d4eea330003e1ed71a125df5767bbdac
Bradz667 <62442850+bradsorph@users.noreply.github.com>
2025-12-04 14:24:13 +0100

    Create AAP inventory and job templates
    Added resources and data objects for AAP job templates.

--- aap_job.tf
+++ aap_job.tf
@@ -1,1 +1,19 @@
+# --- RESOURCE 1: Inventory created in AAP ---
+resource "aap_inventory" "web_inv" {
+  name         = "TF Web Inventory"
+  description  = "Inventory created by Terraform"
+  organization = 2
+}
+
+# --- DATA OBJECT 1: Lookup Job Template (deploy_webserver) ---
+data "aap_job_template" "deploy_webserver" {
+  name              = "deploy_webserver"
+  organization_name = "TechXchangeNL"
+}
+
+# --- DATA OBJECT 2: Lookup Job Template (deploy_website) ---
+data "aap_job_template" "deploy_website" {
+  name              = "deploy_website"
+  organization_name = "TechXchangeNL"
+}
 
