f8d0712d8a01c32bfc558da9f05901c261dbd5f7
Bradz667 <62442850+bradsorph@users.noreply.github.com>
2025-12-04 14:58:49 +0100

    Update inventory name and description

--- aap_inventory.tf
+++ aap_inventory.tf
@@ -1,6 +1,6 @@
 resource "aap_inventory" "bradz_test_inv" {
-  name         = "BRADZ667 - Inventory TF"
-  description  = "A inventory for testing n2"
+  name         = "BRADZ667 - Inventory TF v1"
+  description  = "A inventory for testing n3"
   organization = 2
   variables = jsonencode(
     {
