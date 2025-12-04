b811c34864af7ac912f2dd5d803741eb8e96b1dd
Bradz667 <62442850+bradsorph@users.noreply.github.com>
2025-12-04 14:24:59 +0100

    Rename job templates and inventory for consistency

--- aap_job.tf
+++ aap_job.tf
@@ -1,19 +1,19 @@
 # --- RESOURCE 1: Inventory created in AAP ---
 resource "aap_inventory" "web_inv" {
-  name         = "TF Web Inventory"
+  name         = "BRADZ667 TF Web Inventory"
   description  = "Inventory created by Terraform"
   organization = 2
 }
 
 # --- DATA OBJECT 1: Lookup Job Template (deploy_webserver) ---
 data "aap_job_template" "deploy_webserver" {
-  name              = "deploy_webserver"
+  name              = "BRADZ667-deploy_webserver"
   organization_name = "TechXchangeNL"
 }
 
 # --- DATA OBJECT 2: Lookup Job Template (deploy_website) ---
 data "aap_job_template" "deploy_website" {
-  name              = "deploy_website"
+  name              = "BRADZ667-deploy_website"
   organization_name = "TechXchangeNL"
 }
 
